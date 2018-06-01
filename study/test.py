15f4ec9a846f0520cefb6f9978dc0fa24b15c075
ooojerryooo <ooojerryooo@qq.com>
2018-06-01 15:27:48 +0800

    提交一次

--- study/test.py
+++ study/test.py
@@ -9,7 +9,7 @@ def calc(exp):
     return reduce(lambda acc, x: acc + x, ns)
 
 def main():
-    r = calc('100 + 2000 + 345')
+    r = calc('100 + 200 + 345')
     print('100 + 200 + 345 =', r)
     r = calc('99 + 88 + 7.6')
     print('99 + 88 + 7.6 =', r)
