68d359df022f835141797797146c883c90f9aa49
Jerry大神 <6296467+ooojerryooo@users.noreply.github.com>
2018-06-01 15:11:58 +0800

    Create chain.py

---
+++ chain.py
@@ -0,0 +1,21 @@
+class Chain():
+
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+    def __call__(self, path=''):
+        return Chain('%s/%s' % (self._path, path))
+
+
+    def __str__(self):
+        return self._path
+
+    __repr__ = __str__
+
+if __name__ == '__main__':
+    print(Chain().abc.ddd.nnn('123'))
+
+
